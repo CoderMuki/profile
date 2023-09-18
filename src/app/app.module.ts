6bc5f6cdbabe794a06c9f5d7dd91109383734bb7
CoderMuki <mukilansinbox@gmail.com>
2023-09-18 19:38:10 +0530

    profile-home-initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileHomeComponent } from './profile-home/profile-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileHomeComponent
   ],
   imports: [
     BrowserModule,
