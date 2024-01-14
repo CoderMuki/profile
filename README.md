feb38a208c74adc28d35574784412bb3bf08cf6c
CoderMuki <mukilansinbox@gmail.com>
2024-01-14 23:53:06 +0530

    Skills and Dark Mode

--- README.md
+++ README.md
@@ -28,3 +28,4 @@ To get more help on the Angular CLI use `ng help` or go check out the [Angular C
 
 
 // TODO: Light bulb for light and dark mode
+// TODO: Ripples for light and dark mode
--- src/app/profile-home/profile-home.component.ts
+++ src/app/profile-home/profile-home.component.ts
@@ -7,12 +7,25 @@ import { Component } from '@angular/core';
 })
 export class ProfileHomeComponent {
   showEdu = true;
+  offBulb = true;
 
   urlOpen(url: any){
     window.open(url,'_blank');
+  }
+
+  changetheme(){
+    const body = document.getElementsByTagName('body')[0];
+    if (body.classList.contains('light-theme')){
+      body.classList.remove('light-theme');
+      this.offBulb = true;
+    } else {
+      body.classList.add('light-theme');
+      this.offBulb = false;
+    }
   }
 
 }
 
 
 // TODO: Light bulb for light and dark mode
+// TODO: Ripples for light and dark mode
